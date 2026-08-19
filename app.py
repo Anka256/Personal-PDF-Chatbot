802847817e587b42f6b4462a6b12744f87b1a009
Onur Kansoy <onurkansoy01@gmail.com>
2026-08-19 21:21:25 +0300

    feat: add document embeddings and vector store

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from numpy.char import center
 import streamlit as st
-from rag import load_pdf, split_documents
+from rag import load_pdf, split_documents, create_vector_store
 
 st.html(
     """
@@ -27,10 +26,10 @@ if users_file is not None:
     with st.spinner("PDF loading..."):
         docs = load_pdf(users_file)
         chunks = split_documents(docs)
+        vector_store = create_vector_store(chunks)
+        results = vector_store.similarity_search("Skillset", k=3)
 
     st.success(f"PDF loaded successfully! Total page:{len(docs)}")
 
-    with st.expander("First page content:"):
-        st.write(chunks[0].page_content)
-        st.divider()
-        st.write("**Metadata**", chunks[0].metadata)
+    with st.expander("Results"):
+        st.write(results)
